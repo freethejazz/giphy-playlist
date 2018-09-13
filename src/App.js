3e9b412fee42f6b6770ee49912c65760667c7962
Jonathan Freeman <freethejazz@gmail.com>
2018-09-13 15:41:01 -0500

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">My Favorite Gif</h1>
+        </header>
+        <p className="App-intro">
+          This is where the gif will go
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
